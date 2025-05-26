7899a8bc8a42bc1a00fa5066bf27e4da58ad2177
Nanagranada <NANAGRANADAP@GMAIL.COM>
2025-05-26 11:53:11 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-predeterminada'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+    ALLOWED_EXTENSIONS = {'pdf'}
