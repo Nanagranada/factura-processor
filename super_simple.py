4a0e8bc62685bf5a62a12ecf9b29e5b0a3b9cd7c
Nanagranada <NANAGRANADAP@GMAIL.COM>
2025-05-27 09:43:14 -0500

    Update super_simple.py

--- super_simple.py
+++ super_simple.py
@@ -1,25 +1,25 @@
 cat > super_simple.py << 'EOF'
 from flask import Flask, request
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-return '''
-TEST LOGIN
-
-Usuario:
-Contraseña:
-ENVIAR
-
-'''
+    return '''
+    <h1>TEST LOGIN</h1>
+    <form method="POST" action="/test-login">
+        <p>Usuario: <input type="text" name="username"></p>
+        <p>Contraseña: <input type="password" name="password"></p>
+        <p><button type="submit">ENVIAR</button></p>
+    </form>
+    '''
 
 @app.route('/test-login', methods=['POST'])
 def test_login():
-username = request.form.get('username')
-password = request.form.get('password')
-return f'¡FORMULARIO FUNCIONÓ!Usuario: {username}Contraseña: {password}'
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return f'<h1>¡FORMULARIO FUNCIONÓ!</h1><p>Usuario: {username}<br>Contraseña: {password}</p>'
 
-if name == 'main':
-app.run(debug=True, host='0.0.0.0', port=5000)
-EO
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+EOF
